20ea70fac606185519b2eea403338c80a4f71661
Solorio <solorio_michelle@yahoo.com>
2018-04-17 12:41:30 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,40 @@
 $(document).ready(function(){
+  $("#maincontainer").show();
+  $("#questions").hide();
+  $('#correct').hide();
+  $('#incorrect').hide();
+  $('#unanswered').hide();
+  $('#submit').hide();
+
 
 //Timer
-var time=60;
+ var time=60;
+
+//landing screen
+
+
   //when you click start the timer begins
 $("#start").click(function(){
   start();
   $("#start").hide();
+  $("#questions").show();
+  $('#maincontainer').hide();
+  $('#submit').show();
+
+  
 });
+
+//$("<button>").click(function(){
+  //answers();
+  //$("#correct").show();
+  //$("#incorrect").show();
+  //$("#unanswered").show();
+  //$("#start").hide();
+//});
 
 function start(){
   counter = setInterval(timer, 1000);
+  $("#timer").show();
 };
 
 function timer(){
@@ -18,8 +43,17 @@ function timer(){
 
   if (time===0){
     stop();
-    alert('times up')
+    answers();
+    $('#questions').hide();
+    $('#correct').show();
+    $('#incorrect').show();
+    $('#unanswered').show();
+
+
   };
+
+   
+
 }
 //stoping timer
   function stop(){
@@ -27,24 +61,24 @@ function timer(){
 };
 
 function answers(){
-var q1=document.trivia.q1.value;
-var q2=document.trivia.q1.value;
-var q3=document.trivia.q1.value;
-var q4=document.trivia.q1.value;
-var q5=document.trivia.q1.value;
-var q6=document.trivia.q1.value;
-var q7=document.trivia.q1.value;
-var q8=document.trivia.q1.value;
+var q1 =document.trivia.q1.value;
+var q2 =document.trivia.q2.value;
+var q3 =document.trivia.q3.value;
+var q4 =document.trivia.q4.value;
+var q5 =document.trivia.q5.value;
+var q6 =document.trivia.q6.value;
+var q7 =document.trivia.q7.value;
+var q8 =document.trivia.q8.value;
 
 var correct=0;
 var incorrect=0;
 var unanswered=0;
 
 //question 1
-if (q1===""){
+if (q1==""){
 unanswered++;}
 
-else if(q1==="d"){
+else if(q1=="d"){
   correct++;
 }
 else{
@@ -116,12 +150,17 @@ if (q4===""){
           else if(q8==="b"){
             correct++;
           }
-          else(){
+          else{
             incorrect++
           }
+
+          document.getElementById("correct").innerHTML = ("Correct: " + correct);
+          document.getElementById("incorrect").innerHTML = ("Incorrect: " + incorrect);
+          document.getElementById("unanswered").innerHTML = ("Unanswered: " + unanswered);
+
+
 }
+
+
 });
-
-
-
 
